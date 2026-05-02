673c6bf53b8ef1d6518563547879a791abfa7b13
stevenschiff <stevenmschiff@gmail.com>
2026-05-02 13:52:52 -0400

    fixed error

--- src/client/java/com/steveplays/superawesomemod/XrayLineRenderType.java
+++ src/client/java/com/steveplays/superawesomemod/XrayLineRenderType.java
@@ -7,16 +7,17 @@ import net.minecraft.client.renderer.rendertype.LayeringTransform;
 import net.minecraft.client.renderer.rendertype.OutputTarget;
 import net.minecraft.client.renderer.rendertype.RenderSetup;
 import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.resources.Identifier;
 
 public final class XrayLineRenderType {
 
     private static final RenderPipeline PIPELINE = RenderPipeline.builder(RenderPipelines.LINES_SNIPPET)
-        .withLocation("superawesomemod:lines_xray")
+        .withLocation(Identifier.fromNamespaceAndPath("superawesomemod", "lines_xray"))
         .withDepthTestFunction(DepthTestFunction.NO_DEPTH_TEST)
         .build();
 
     public static final RenderType LINES_XRAY = RenderType.create(
-        "superawesomemod:lines_xray",
+        "lines_xray",
         RenderSetup.builder(PIPELINE)
             .setLayeringTransform(LayeringTransform.VIEW_OFFSET_Z_LAYERING)
             .setOutputTarget(OutputTarget.ITEM_ENTITY_TARGET)
