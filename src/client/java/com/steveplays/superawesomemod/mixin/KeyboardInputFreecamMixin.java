2a9438c656d336ac03be6816379f5047ac1bc206
stevenschiff <stevenmschiff@gmail.com>
2026-05-06 16:03:42 -0400

    fixed

--- src/client/java/com/steveplays/superawesomemod/mixin/KeyboardInputFreecamMixin.java
+++ src/client/java/com/steveplays/superawesomemod/mixin/KeyboardInputFreecamMixin.java
@@ -1,20 +1,17 @@
 package com.steveplays.superawesomemod.mixin;
 
 import com.steveplays.superawesomemod.FreecamData;
+import net.minecraft.client.player.ClientInput;
 import net.minecraft.client.player.KeyboardInput;
 import net.minecraft.world.entity.player.Input;
 import net.minecraft.world.phys.Vec2;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(KeyboardInput.class)
-public abstract class KeyboardInputFreecamMixin {
-
-    @Shadow public Input keyPresses;
-    @Shadow protected Vec2 moveVector;
+public abstract class KeyboardInputFreecamMixin extends ClientInput {
 
     @Inject(method = "tick", at = @At("TAIL"))
     private void superawesomemod$suppressInputDuringFreecam(CallbackInfo ci) {
