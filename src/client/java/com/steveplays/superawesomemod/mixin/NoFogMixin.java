e5a2e729589b788c9c1489a5666ad413a767990f
stevenschiff <stevenmschiff@gmail.com>
2026-08-03 14:56:05 -0400

    udpate

--- src/client/java/com/steveplays/superawesomemod/mixin/NoFogMixin.java
+++ src/client/java/com/steveplays/superawesomemod/mixin/NoFogMixin.java
@@ -1,32 +1,23 @@
 package com.steveplays.superawesomemod.mixin;
 
-import com.mojang.blaze3d.buffers.GpuBufferSlice;
 import com.steveplays.superawesomemod.NoFogData;
-import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.fog.FogRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 /**
  * Disables world fog (underwater, nether, etc.) by replacing the WORLD fog
- * buffer with the NONE fog buffer when the No Fog feature is enabled.
+ * mode with NONE when the No Fog feature is enabled.
  */
-@Mixin(GameRenderer.class)
+@Mixin(FogRenderer.class)
 public class NoFogMixin {
 
-    @Redirect(
-        method = "updateCamera",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/renderer/fog/FogRenderer;getBuffer(Lnet/minecraft/client/renderer/fog/FogRenderer$FogMode;)Lcom/mojang/blaze3d/buffers/GpuBufferSlice;",
-            ordinal = 0
-        )
-    )
-    private GpuBufferSlice superawesomemod$noFog(FogRenderer renderer, FogRenderer.FogMode mode) {
-        if (NoFogData.isEnabled()) {
-            return renderer.getBuffer(FogRenderer.FogMode.NONE);
+    @ModifyVariable(method = "getBuffer", at = @At("HEAD"), argsOnly = true)
+    private FogRenderer.FogMode superawesomemod$noFog(FogRenderer.FogMode mode) {
+        if (NoFogData.isEnabled() && mode == FogRenderer.FogMode.WORLD) {
+            return FogRenderer.FogMode.NONE;
         }
-        return renderer.getBuffer(mode);
+        return mode;
     }
 }
